ea02e46b7e9a18b56bfe0bb79f5edc0c89c87503
SantiagoHC123 <santiagohenao98@hotmail.com>
2023-11-08 04:29:51 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,11 @@ import { useApi, ApiProvider } from './Contexto/ContextApi.jsx';
     <>
      <div className="Contenedor">
       <h1>Api para generar personas Random</h1>
+      <img src='https://e7.pngegg.com/pngimages/635/811/png-clipart-men-and-women-mysterious-figure-with-a-question-mark-question-mark-mysterious-person.png' />
+      <div>
       <button onClick={() => fetchRandomUser('male')}>Generar Hombre</button>
-        <button onClick={() => fetchRandomUser('female')}>Generar Mujer</button>
+      <button onClick={() => fetchRandomUser('female')}>Generar Mujer</button>
+      </div>
       {loading ? (
         <p>Cargando...</p>
       ) : (
